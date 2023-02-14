cbbad342e4f28e51d4ee7aa0a44a0fff8777b500
Alexander <amoshenin@mail.ru>
2023-02-14 12:36:16 +0300

    add modal window auth + style

--- js/script.js
+++ js/script.js
@@ -1,11 +1,2 @@
-baseURL = "http://localhost:8080/api"
+baseURL = "http://localhost:8080/api";
 
-window.addEventListener('load', () => {
-  console.log('Страница загрузилась');
-  getLoadRequest(1);
-})
-
-async function getLoadRequest(id) {
-  await axios.get(`${baseURL}/tasks/${id}`)
-  .then(res => console.log(res.data));
-}
