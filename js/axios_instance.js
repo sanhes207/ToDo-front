f2703e063cfdfed2825eb13d0a985df1e2ebffa7
Alexander <amoshenin@mail.ru>
2023-02-16 11:13:28 +0300

    axios instance: made userID trackable via cookie. Tasks are returned by userID and category. script: added appearance of category cards after user definition

--- js/axios_instance.js
+++ js/axios_instance.js
@@ -1,27 +1,42 @@
+import Cookies from "../node_modules/js-cookie/dist/js.cookie.min.mjs";
+
 // Создаем путь до статичного localhost
 const instance = axios.create({
   baseURL: "http://localhost:8080/api"
 });
 
-
-// 
+// Возвращаем id авторизованного пользователя
 async function getUser(userName) {
-  instance.get(`/user/${userName}`)
-  .then(res => console.log(res.data.id))
-  .catch(err => console.log(err))
+  await instance.get(`/user/${userName}`)
+  .then(res => Cookies.set('userID', res.data.id));
 }
 
-async function getTask(userID) {
+async function getTask(categoryID) {
+  let response;
   await instance.get('/task', {
     headers: {
-      'user_id': userID
+      'user_id': Cookies.get('userID'),
+      'category_id': categoryID
     }
   })
-  .then(res => {
-    console.log(res);
+  .then(res => response = res.data)
+  .catch(err => response = err)
+
+  return response;
+}
+
+async function getCategorys() {
+  let response;
+  await instance.get('/category', {
+    headers: {
+      'user_id': Cookies.get('userID')
+    }
   })
-  .catch(err => console.log(err))
+  .then(res => response = res.data)
+  .catch(err => response = err)
+
+  return response;
 }
 
 
-export {getUser, getTask};
+export {getUser, getTask, getCategorys};
--- js/script.mjs
+++ js/script.mjs
@@ -1,4 +1,4 @@
-import {getUser, getTask} from './axios_instance.js'; 
+import {getUser, getTask, getCategorys} from './axios_instance.js'; 
 
 // Переменные для кнопок
 const btnAuth = document.querySelector('.auth__accept-button');
@@ -6,12 +6,49 @@ const btnAddTask = document.querySelector('.header__add-task');
 
 
 // Авторизация пользователей
-btnAuth.addEventListener('click', () => {
+btnAuth.addEventListener('click', async () => {
   const userName = document.querySelector('.auth__title');
   const modalAuth = document.querySelector('.modal__auth')
 
-  const userID =  getUser(userName.value);
-  const taskList = getTask(userID);
+  await getUser(userName.value);
+  const categorysList = await getCategorys();
+
+  categorysList.forEach(category => {
+    appendCard(category)
+  })
 
   modalAuth.classList.add('hidden')
 });
+
+// Создаем передаваемый элемент с передаваемым классом 
+function createElementWithClass(el, className) {
+  const element = document.createElement(el);
+  element.classList.add(className);
+  return element;
+}
+
+// Добавить новую карточку
+async function appendCard(category) {
+
+  const main = document.querySelector('.main');
+
+  const card = createElementWithClass('div', 'card');
+  card.id = category.id;
+
+  const cardHeader = createElementWithClass('div', 'card__header');
+
+  const cardTitle = createElementWithClass('h2', 'card__title');
+  cardTitle.textContent = `${category.title}`;
+
+  const cardTask = createElementWithClass('div', 'card');
+
+  cardHeader.append(cardTitle);
+  card.append(cardHeader);
+  card.append(cardTask);
+  main.append(card);
+}
+
+// Добавить новую задачу
+async function appendTask() {
+
+}
